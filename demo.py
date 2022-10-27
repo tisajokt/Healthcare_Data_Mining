ec9522e5644260d95e25cab2be26c67aff2257db
Timothy Walker <james.and.tisa@gmail.com>
2022-10-27 10:33:43 -0400

    Add todo list

--- demo.py
+++ demo.py
@@ -2,6 +2,18 @@
 # Author: Timothy Walker <james.and.tisa@gmail.com>
 # Usage: python3 demo.py
 
+# TODO
+# 1) Interactive menu for model training
+# 2) Options should include:
+#	- naive/complex stats display
+#	- one model vs. array of models
+#	- training data size
+#	- data pipeline for processing
+# 3) Interactive explanation of data validation
+#	- outlier removal
+#	- handling of null values
+#	- handling of categorical data fields
+
 def main():
 	pass
 
